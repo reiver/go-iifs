a0dd25eb0dba5da3a0c4c86b109fdcebaf50937b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-21 07:39:33 -0700

    iifs.MountPoint.location()

--- mountpoint_location_test.go
+++ mountpoint_location_test.go
@@ -14,12 +14,12 @@ func TestMountPointLocation(t *testing.T) {
 		{
 			Content: []byte(nil),
 			MountPath: "/home/joeblow/workspaces/articles/.ii/content",
-			Expected:  "/home/joeblow/workspaces/articles/.ii/content/sha-256.sha-256/5d/0r5dfxE0E2Vx1m5kdm0AH2V505HE2kkfCC0mH15m4545f55Cf4mE41kHmf5d4Ck45x",
+			Expected:  "/home/joeblow/workspaces/articles/.ii/content/SHA-256.SHA-256/5d/0r5dfxE0E2Vx1m5kdm0AH2V505HE2kkfCC0mH15m4545f55Cf4mE41kHmf5d4Ck45x",
 		},
 		{
 			Content: []byte{},
 			MountPath: "/home/joeblow/workspaces/articles/.ii/content",
-			Expected:  "/home/joeblow/workspaces/articles/.ii/content/sha-256.sha-256/5d/0r5dfxE0E2Vx1m5kdm0AH2V505HE2kkfCC0mH15m4545f55Cf4mE41kHmf5d4Ck45x",
+			Expected:  "/home/joeblow/workspaces/articles/.ii/content/SHA-256.SHA-256/5d/0r5dfxE0E2Vx1m5kdm0AH2V505HE2kkfCC0mH15m4545f55Cf4mE41kHmf5d4Ck45x",
 		},
 
 
@@ -27,22 +27,22 @@ func TestMountPointLocation(t *testing.T) {
 		{
 			Content: []byte("apple"),
 			MountPath: "/home/joeblow/workspaces/articles/.ii/content",
-			Expected:  "/home/joeblow/workspaces/articles/.ii/content/sha-256.sha-256/2x/0r2xf1VCAAdbHCmHb04mdffAkVfff0f5bkmf5x4fbbkCxmdk1A0E4Eb05410dHbCkE",
+			Expected:  "/home/joeblow/workspaces/articles/.ii/content/SHA-256.SHA-256/2x/0r2xf1VCAAdbHCmHb04mdffAkVfff0f5bkmf5x4fbbkCxmdk1A0E4Eb05410dHbCkE",
 		},
 		{
 			Content: []byte("BANANA"),
 			MountPath: "/home/joeblow/workspaces/articles/.ii/content",
-			Expected:  "/home/joeblow/workspaces/articles/.ii/content/sha-256.sha-256/10/0r1025104HmmbE0kAkA51VAdVxC4fkd1AC4C1xH2Ek2Vk2Hd4fkEE0f1EdVx1411df",
+			Expected:  "/home/joeblow/workspaces/articles/.ii/content/SHA-256.SHA-256/10/0r1025104HmmbE0kAkA51VAdVxC4fkd1AC4C1xH2Ek2Vk2Hd4fkEE0f1EdVx1411df",
 		},
 		{
 			Content: []byte("Cherry"),
 			MountPath: "/home/joeblow/workspaces/articles/.ii/content",
-			Expected:  "/home/joeblow/workspaces/articles/.ii/content/sha-256.sha-256/A0/0rA0241kHC40m5fV0x5E51d4fH0bCkE2C25ddkHmfEVmV2EV4xb55bVxdmbdAA2bkk",
+			Expected:  "/home/joeblow/workspaces/articles/.ii/content/SHA-256.SHA-256/A0/0rA0241kHC40m5fV0x5E51d4fH0bCkE2C25ddkHmfEVmV2EV4xb55bVxdmbdAA2bkk",
 		},
 		{
 			Content: []byte("dATE"),
 			MountPath: "/home/joeblow/workspaces/articles/.ii/content",
-			Expected:  "/home/joeblow/workspaces/articles/.ii/content/sha-256.sha-256/2x/0r2xEmd02E0f0kf41CddEx0fVVHEmCk1C1VCbCCEH04A1mAb54AbxkHbxVmd4EEmb5",
+			Expected:  "/home/joeblow/workspaces/articles/.ii/content/SHA-256.SHA-256/2x/0r2xEmd02E0f0kf41CddEx0fVVHEmCk1C1VCbCCEH04A1mAb54AbxkHbxVmd4EEmb5",
 		},
 
 
@@ -50,7 +50,7 @@ func TestMountPointLocation(t *testing.T) {
 		{
 			Content: []byte("Hello world!"),
 			MountPath: "/home/joeblow/workspaces/articles/.ii/content",
-			Expected:  "/home/joeblow/workspaces/articles/.ii/content/sha-256.sha-256/Vk/0rVkH2kV05m4E0HkbHmkk5VbVE1V4V25CE1HVHVm1EdxdV00VxxE5kCb1xf1C25E2V",
+			Expected:  "/home/joeblow/workspaces/articles/.ii/content/SHA-256.SHA-256/Vk/0rVkH2kV05m4E0HkbHmkk5VbVE1V4V25CE1HVHVm1EdxdV00VxxE5kCb1xf1C25E2V",
 		},
 	}
 
